323cc72807074eaf70a61c2a6bf86f4730e55d6e
Mrrwmix <seidoc@gmail.com>
2019-08-11 14:16:08 -0700

    Skeleton games/movies/books/form/home sites

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,17 +3,29 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.render("home");
   });
 
+  app.get("/games", function(req, res) {
+    res.render("games");
+  });
+
+  app.get("/movies", function(req, res) {
+    res.render("movies");
+  });
+
+  app.get("/books", function(req, res) {
+    res.render("books");
+  });
+
+  app.get("/form", function(req, res) {
+    res.render("form");
+  });
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.render("example", {
         example: dbExample
       });
