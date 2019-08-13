31375cf67ea1a2796c125ed3a07370fd56418e67
Mrrwmix <seidoc@gmail.com>
2019-08-12 20:32:25 -0700

    Associated the tables

--- models/users.js
+++ models/users.js
@@ -1,6 +1,30 @@
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("Users", {
-    name: DataTypes.STRING
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [2, 70]
+      }
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [2, 70]
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   });
+
+  User.associate = function(models) {
+    User.hasMany(models.Review, {
+      onDelete: "cascade"
+    });
+  };
   return User;
 };
