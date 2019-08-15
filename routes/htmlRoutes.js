4f9e88d05f3d58532ada18015fb72a8cd27a3ea4
Matthew Winemiller <seidoc@gmail.com>
2019-08-14 22:57:18 -0700

    No more semantic, doing a test

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -38,6 +38,9 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/test", (req, res) => {
+    res.render("test");
+  })
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
