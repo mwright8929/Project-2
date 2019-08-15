32adec85c39d5ea9d65024b05c1dc84b3f46971b
Mrrwmix <seidoc@gmail.com>
2019-08-15 12:12:34 -0700

    On step 4

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -38,6 +38,10 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/signup", function(req, res) {
+    res.render("signup");
+  });
+
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
